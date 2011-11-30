708d2c89810571af827ff092c284bff41f345856
Daniel Bernal <jdbernal@fullsail.edu>
2011-11-30 18:12:37 -0500

    main.js set up

--- JS/main.js
+++ JS/main.js
@@ -3,4 +3,27 @@
 //VFW 1112
 //12/01/2011		
 
+// DOM Content Loaed.
+window.addEventListener("DOMContentLoaded", function(){
 
+	//getElementById function
+	function $(x) {
+		var theElement = document.getElementById(x);
+		return theElement;
+	};
+	
+	
+	//Variable defaults
+	
+	
+	
+	
+	//Set link & sudmit click Events
+
+
+
+
+
+
+
+});
