20cf50c343e1c12c8bd0fae545a95bc72f39a15c
Daniel Bernal <jdbernal@fullsail.edu>
2011-11-30 19:17:33 -0500

    main.js update03

--- JS/main.js
+++ JS/main.js
@@ -12,21 +12,39 @@ window.addEventListener("DOMContentLoaded", function(){
 		return theElement;
 	};
 	
+	// Field Elements
+	
+	// Select Field
+	function selectType() {
+		var formTag = document.getElementsByTagName("form");	//array
+			selectLi = $("selectType")
+			makeSelect = document.createElement("selectType");
+			makeSelect.setAttribute("id", "pType")
+		 for (var i=0, j=projectType.length; i<j; i++) {
+		 	var makeOption = document.createElement("option");
+		 	var optText = projectType[i];
+			makeOption.setAttribute("value", optText);
+			makeOption.innerHTML = optText;
+			makeSelect.appendChild(makeOption);
+		};
+		selectLi.appendChild(makeSelect);
+	};
 	
 	//Variable defaults
 	
-	var projectType = ["-- Choose A Type --", "Site Development", "Building Envelope", "Interior Design"];
-	
+	var projectType = [" -- Choose A Type -- ", "Site Development", "Building Envelope", "Interior Design"];
+	selectType();
 
 	
 	
-	//Set link & sudmit click Events
-	var displayLink = $("display");
-	displayLink.addEventListener("click", getData);
-	var clearLink = $("clear");
-	clearLink.addEventListener("click", clearLocal);
-	var saveLink = $("submit");
-	saveLink.addEventListener("click", storeData);
+	//Set link & submit click Events
+	
+//	var displayLink = $("display");
+//	displayLink.addEventListener("click", getData);
+//	var clearLink = $("clear");
+//	clearLink.addEventListener("click", clearLocal);
+//	var saveLink = $("submit");
+//	saveLink.addEventListener("click", storeData);
 
 
 
