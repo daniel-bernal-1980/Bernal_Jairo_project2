a5f89f1371bf28109f713e379648626684ea1b71
Daniel Bernal <jdbernal@fullsail.edu>
2011-11-30 21:13:42 -0500

    main.js update05

--- JS/main.js
+++ JS/main.js
@@ -30,31 +30,59 @@ window.addEventListener("DOMContentLoaded", function(){
 		selectLi.appendChild(makeSelect);
 	};
 	
+	//Value of selected radio button.
+	
+	function getSelectedRadio() {
+		var radios = document.forms[0].models;
+		 for (var i=0; i<radios.length; i++) {
+		 	if(radios[i].checked){
+		 		workValue = radios[i].value;
+		 	};
+		 };
+	};
+	
+	//Value of selected checkbox.
+	
+	function getCheckboxValue() {
+		var checkboxes = document.forms[0].drawings;
+			for (var i=0; i<checkboxes.length; i++) {
+				if (checkboxes[i].checked){
+					dwgValue = 	checkboxes[i].value;		
+				}else{
+					dwgValue = "No Drawings Needed!"
+			};
+	};
+	
+	
+	
 	// Store Data
+	
 	function storeData() {
 		var id				= Math.floor(Math.random()*10000001);
+		getSelectedRadio();
+		getCheckboxValue();
 		var item			= {};
-			item.pType		= ["Project", $("pType").value];
+			item.pType		= ["Project Type:", $("pType").value];
 			item.pName		= ["Project Name:", $("pName").value];
 			item.pNum		= ["Project Number:", $("pNum").value];
 			item.dName		= ["Designer Name:", $("dName").value];
 			item.dateComp	= ["Completion Date:", $("dateComp").value];
-//			item.dwg		= ["Drawings:", dwgValue];
+			item.dwg		= ["Drawings:", dwgValue];
 			item.dComt		= ["Drawings Comments:", $("dComments").value];
-//			item.work3d		= ["3D Work:", workValue];
+			item.work3d		= ["3D Work:", workValue];
 			item.wComt		= ["3D Work Comments:", $("wComments").value];
 			item.rend		= ["Renderings:", $("rend").value];
 			
 		localStorage.setItem(id, JSON.stringify(item));
 		alert("Contact Saved!");
-			
-			
-	
 	};
 	
 	//Variable defaults
 	
-	var projectType = [" -- Choose A Type -- ", "Site Development", "Building Envelope", "Interior Design"];
+	var projectType = [" -- Choose A Type -- ", "Site Development", "Building Envelope", "Interior Design"],
+		workValue,
+		dwgValue = "No Drawings Needed!"
+	;
 	selectType();
 
 	
